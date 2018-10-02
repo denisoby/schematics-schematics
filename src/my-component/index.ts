d9110cdceed70fa9f8d2da12b3e8a2d2d3d6a24a
denis.obydennykh <dobydennykh@griddynamics.com>
2018-10-02 20:15:18 +0300

    stub code for working with modules and imports using angular schematics api

--- src/my-component/index.ts
+++ src/my-component/index.ts
@@ -15,6 +15,39 @@ export function myComponent(options: any): Rule {
   });
 
   return chain([
+    (tree: Tree, _context: SchematicContext) => {
+      tree;
+    },
+    (tree: Tree, _context: SchematicContext) => {
+      tree;
+      // getPackageJsonDependency
+      // addPackageJsonDependency
+      // _context.addTask(new NodePPackageInstallTask());
+    },
+    (tree: Tree, _context: SchematicContext) => {
+      tree;
+      // const project = getProject(host, options.project);
+      // if (options.path === undefined) {
+      //   options.path = buildDefaultPath(project);
+      // }
+      // options.module ?= findModuleFromOptions(host, options);
+      // options.module = options.module.replace(options.path, '');
+    },
+    (tree: Tree, _context: SchematicContext) => {
+      // const modulePath = options.path + options.module;
+      // const rule = addImportToNgModule(modulePath, 'SomeModule', 'some-package');
+      // return rule(host, _context);
+      tree;
+    },
+    (tree: Tree, _context: SchematicContext) => {
+      // const modulePath = options.path + options.module;
+      // const rule = addImport(modulePath, 'symbolName', 'package-name', true);
+      // "true" is supported by patched version of import
+      tree;
+    },
+    (tree: Tree, _context: SchematicContext) => {
+      tree;
+    },
     externalSchematic('@schematics/angular', 'component', componentOptions),
     (tree: Tree, _context: SchematicContext) => {
       tree.getDir(options.sourceDir)
